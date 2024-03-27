178e61b8f8e822db073fd130c2eef66c30a57ee0
Kirsi Mak <ying987554@hotmail.com>
2024-03-28 01:29:20 +0800

    Create JsonHandlerThread.java

---
+++ app/src/main/java/com/example/myapplication/api/JsonHandlerThread.java
@@ -0,0 +1,89 @@
+package com.example.myapplication.api;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class JsonHandlerThread extends Thread {
+    private static final String TAG = "JsonHandlerThread";
+    // URL to get contacts JSON file
+    private static String jsonUrl = "https://api.openweathermap.org/data/2.5/weather?lat=37.421998333333335&lon=-122.084&appid=0afc41116086771ceea4c08d88916501";
+
+    // send request to the url, no need to be changed
+    public static String makeRequest() {
+        String response = null;
+        try {
+            URL url = new URL(jsonUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+
+            // Read the response
+            InputStream in = new BufferedInputStream(conn.getInputStream());
+            response = inputStreamToString(in);
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "MalformedURLException: " + e.getMessage());
+        } catch (ProtocolException e) {
+            Log.e(TAG, "ProtocolException: " + e.getMessage());
+        } catch (IOException e) {
+            Log.e(TAG, "IOException: " + e.getMessage());
+        } catch (Exception e) {
+            Log.e(TAG, "Exception: " + e.getMessage());
+        }
+        return response;
+    }
+
+    // download of JSON file from the url to the app
+    private static String inputStreamToString(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = "";
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append('\n');
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "IOException: " + e.getMessage());
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                Log.e(TAG, "IOException: " + e.getMessage());
+            }
+        }
+        return sb.toString();
+    }
+
+    public void run() {
+        // "contactStr" variable store the json file content
+        String contactStr = makeRequest();
+        Log.e(TAG, "Response from url: " + contactStr);
+
+        if (contactStr != null) {
+            try {
+                JSONObject jsonObj = new JSONObject(contactStr);
+
+
+                CurrentWeather.res= jsonObj.toString();
+
+
+            } catch (final JSONException e) {
+                Log.e(TAG, "Json parsing error: " + e.getMessage());
+            }
+        } else {
+            Log.e(TAG, "Couldn't get json from server.");
+        }
+    }
+}
