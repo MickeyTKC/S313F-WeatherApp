e1a7b53173c608ac6564f83994022c810777351c
MickeyTKC <tt51145505@gmail.com>
2024-04-04 17:24:55 +0800

    Add refresh button

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -86,8 +86,9 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Refreshing ...", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                getLocation();
             }
         });
         // NavBar and Items
