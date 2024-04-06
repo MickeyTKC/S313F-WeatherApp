47dd1ef54cd6fface63e923505e28629c3a87e62
MickeyTKC <tt51145505@gmail.com>
2024-04-06 20:39:53 +0800

    Update Navbar

--- app/src/main/java/com/example/myapplication/ui/forecast/ForecastFragment.java
+++ app/src/main/java/com/example/myapplication/ui/forecast/ForecastFragment.java
@@ -1,6 +1,43 @@
 package com.example.myapplication.ui.forecast;
 
-import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
-public class ForecastFragment  extends Fragment {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.myapplication.R;
+import com.example.myapplication.databinding.FragmentForecastBinding;
+import com.example.myapplication.databinding.FragmentGalleryBinding;
+import com.example.myapplication.model.ForecastWeather;
+import com.example.myapplication.ui.gallery.GalleryViewModel;
+
+public class ForecastFragment extends Fragment {
+    private FragmentForecastBinding binding;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        ForecastViewModel forecastViewModel =
+                new ViewModelProvider(this).get(ForecastViewModel.class);
+        binding = FragmentForecastBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        final ListView listView = binding.forecastList;
+        forecastViewModel.getList().observe(getViewLifecycleOwner(),list->{
+            SimpleAdapter adapter = new SimpleAdapter(
+                    this.getContext(),
+                    ForecastWeather.data,
+                    R.layout.forecast_list,
+                    new String[] { ForecastWeather.DESCRIPTION, ForecastWeather.TEMP },
+                    new int[] { R.id.weather, R.id.temp }
+            );
+
+            listView.setAdapter(adapter);
+        });
+        return root;
+    }
 }
--- app/src/main/java/com/example/myapplication/ui/forecast/ForecastViewModel.java
+++ app/src/main/java/com/example/myapplication/ui/forecast/ForecastViewModel.java
@@ -1,6 +1,22 @@
 package com.example.myapplication.ui.forecast;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.myapplication.model.ForecastWeather;
+
+import java.util.*;
+
 public class ForecastViewModel  extends ViewModel {
+    private MutableLiveData<ArrayList<HashMap<String, String>>> forecastList;
+
+    public ForecastViewModel() {
+        forecastList = new MutableLiveData<>();
+        forecastList.setValue(ForecastWeather.data);
+    }
+
+    public LiveData<ArrayList<HashMap<String, String>>> getList() {
+        return forecastList;
+    }
 }
