8a4673fcbef7e4a8b047439fcef52a4d0d88beaa
Kirsi Mak <ying987554@hotmail.com>
2024-04-06 19:46:24 +0800

    forecast base

---
+++ app/src/main/java/com/example/myapplication/ui/forecast/ForecastFragment.java
@@ -0,0 +1,6 @@
+package com.example.myapplication.ui.forecast;
+
+import androidx.fragment.app.Fragment;
+
+public class ForecastFragment  extends Fragment {
+}
---
+++ app/src/main/java/com/example/myapplication/ui/forecast/ForecastViewModel.java
@@ -0,0 +1,6 @@
+package com.example.myapplication.ui.forecast;
+
+import androidx.lifecycle.ViewModel;
+
+public class ForecastViewModel  extends ViewModel {
+}
